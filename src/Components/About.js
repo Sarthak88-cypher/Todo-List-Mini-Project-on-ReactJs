0bce165da1d72d5036dc91c8ff709dda37f0a275
Sai Sarthak Mohapatra <62335086+Sarthak88-cypher@users.noreply.github.com>
2021-06-07 17:37:05 +0530

    Add files via upload

---
+++ src/Components/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About  ()  {
+    return (
+        <div>
+            This is About Us
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda earum temporibus, cum error, sed, similique voluptatum veniam odio impedit hic ipsum sunt et labore cumque! Aperiam autem dicta delectus dolore!</p>
+        </div>
+    )
+}
